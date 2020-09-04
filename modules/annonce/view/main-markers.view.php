c2c1bb4aabb3b4011b5ce1be73fe3a9e900faf2d
eoxia-amandine <amandine@eoxia.com>
2020-09-04 12:04:07 +0200

    Correction d'un bug supprimant le dernier marker

--- modules/annonce/view/main-markers.view.php
+++ modules/annonce/view/main-markers.view.php
@@ -14,26 +14,28 @@ namespace annonces;
 defined( 'ABSPATH' ) || exit;
 
 if ( ! empty( $annonces_map_query->posts ) ) :
-	foreach ( $annonces_map_query->posts as $annonce ) : ?>
-
-		<marker id="<?php echo esc_html( $annonce->ID ); ?>"
-		lat="<?php echo esc_html( $annonce->address['lat'] ); ?>"
-		lng="<?php echo esc_html( $annonce->address['lng'] ); ?>"
-		taxonomy="<?php echo esc_attr( $annonce->tax ); ?>"
-		pin="<?php echo ( ! empty( $annonce->pin ) ) ? esc_html( $annonce->pin ) : 'red'; ?>">
+	echo '<pre>';
+	print_r($annonces_map_query->posts);
+	echo '</pre>';
+	foreach ( $annonces_map_query->posts as $marker_annonce ) : ?>
+		<marker id="<?php echo esc_html( $marker_annonce->ID ); ?>"
+		lat="<?php echo esc_html( $marker_annonce->address['lat'] ); ?>"
+		lng="<?php echo esc_html( $marker_annonce->address['lng'] ); ?>"
+		taxonomy="<?php echo esc_attr( $marker_annonce->tax ); ?>"
+		pin="<?php echo ( ! empty( $marker_annonce->pin ) ) ? esc_html( $marker_annonce->pin ) : 'red'; ?>">
 			<div class="marker-container wpeo-gridlayout grid-2">
 				<figure class="thumbnail">
 					<?php
-					$thumb_id  = get_post_thumbnail_id( $annonce->ID );
+					$thumb_id  = get_post_thumbnail_id( $marker_annonce->ID );
 					$thumb_url = wp_get_attachment_image_url( $thumb_id, 'thumbnail' );
 					?>
 					<img src="<?php echo esc_url( $thumb_url ); ?>" />
 				</figure>
 				<div class="content">
-					<p class="content-title"><?php echo esc_html( $annonce->post_title ); ?></p>
-					<p class="content-description"><?php echo wp_trim_words( $annonce->post_content, 10, '...' ); // WPCS: XSS ok. ?></p>
+					<p class="content-title"><?php echo esc_html( $marker_annonce->post_title ); ?></p>
+					<p class="content-description"><?php echo wp_trim_words( $marker_annonce->post_content, 10, '...' ); // WPCS: XSS ok. ?></p>
 					<ul class="content-datas">
-						<?php foreach ( $annonce->datas as $data ) : ?>
+						<?php foreach ( $marker_annonce->datas as $data ) : ?>
 							<?php if ( ! empty( $data['content'] ) ) : ?>
 								<li>
 										<span class="data-icon"><i class="fas fa-<?php echo esc_html( $data['icon'] ); ?>"></i></span>
@@ -43,10 +45,9 @@ if ( ! empty( $annonces_map_query->posts ) ) :
 							<?php endif; ?>
 						<?php endforeach; ?>
 					</ul>
-					<a href="<?php echo esc_url( get_permalink( $annonce->ID ) ); ?>" class="wpeo-button button-main button-size-small"><?php esc_html_e( 'See', 'annonces' ); ?></a>
+					<a href="<?php echo esc_url( get_permalink( $marker_annonce->ID ) ); ?>" class="wpeo-button button-main button-size-small"><?php esc_html_e( 'See', 'annonces' ); ?></a>
 				</div>
 			</div>
 		</marker> <?php
-
 	endforeach;
 endif;
